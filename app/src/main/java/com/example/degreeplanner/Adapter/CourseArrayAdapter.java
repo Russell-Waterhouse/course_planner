bd3089ee9d428a53466b128abfc47c43158e1118
Russell <Russell.L.Waterhouse@gmail.com>
2019-06-21 23:23:00 -0700

    changed to a listview system to leverage onItemClickListener method
    Signed-off-by: Russell <Russell.L.Waterhouse@gmail.com>

---
+++ app/src/main/java/com/example/degreeplanner/Adapter/CourseArrayAdapter.java
@@ -0,0 +1,58 @@
+package com.example.degreeplanner.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.degreeplanner.Database.CourseEntity;
+import com.example.degreeplanner.R;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class CourseArrayAdapter extends ArrayAdapter<CourseEntity> {
+
+    private Context mContext;
+
+    public CourseArrayAdapter(Context context, int resource, List<CourseEntity> courses){
+        super(context, resource, courses);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView==null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.course_layout, parent, false);
+        }
+        CourseEntity course = this.getItem(position);
+        TextView courseName = convertView.findViewById(R.id.course_name);
+        LinearLayout fall = convertView.findViewById(R.id.offered_in_fall);
+        LinearLayout spring = convertView.findViewById(R.id.offered_in_spring);
+        LinearLayout summer= convertView.findViewById(R.id.offered_in_summer);
+        TextView prereq1 = convertView.findViewById(R.id.prereq_1);
+        TextView prereq2 = convertView.findViewById(R.id.prereq_2);
+        TextView prereq3 = convertView.findViewById(R.id.prereq_3);
+        if(course != null) {
+            courseName.setText(course.getCourseName());
+            prereq1.setText(course.getPrereq1Str());
+            prereq2.setText(course.getPrereq2Str());
+            prereq3.setText(course.getPrereq3Str());
+            if(!course.isOfferedInFall())
+                fall.setVisibility(View.INVISIBLE);
+            if(!course.isOfferedInSpring())
+                spring.setVisibility(View.INVISIBLE);
+            if(!course.isOfferedInSummer())
+                summer.setVisibility(View.INVISIBLE);
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/degreeplanner/MainActivity.java
+++ app/src/main/java/com/example/degreeplanner/MainActivity.java
@@ -4,36 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-import com.example.degreeplanner.Adapter.RecyclerViewAdapter;
+import com.example.degreeplanner.Adapter.CourseArrayAdapter;
 import com.example.degreeplanner.Database.CourseEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     MainViewModel viewModel;
     List<CourseEntity> courseList;
+    ArrayList<CourseEntity> unscheduledCourses;
+    ArrayList<CourseEntity> scheduledCourses;
+    View selectedCourse;
+
+    private int selectedCoursePosition;
+    private ListView selectedListView;
+    ListView bottomListView ;
+    ListView fall_1_ListView ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomListView = findViewById(R.id.bottom_list_view);
+        fall_1_ListView = findViewById(R.id.fall_1);
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        unscheduledCourses = new ArrayList<>();
         LiveData<List<CourseEntity>> courseListLive = viewModel.getAllCourses();
         courseListLive.observe(this, new Observer<List<CourseEntity>>() {
             @Override
             public void onChanged(List<CourseEntity> courseEntities) {
                 courseList = courseEntities;
-                initRecyclerView();
+                initListView();
             }
         });
 
-//        initRecyclerView();
+//        bottomListView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                selectedListView = bottomListView;
+//            }
+//        });
+
+//        fall_1_ListView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if(!selectedListView.equals(fall_1_ListView)){
+//
+//                }
+//                selectedListView = fall_1_ListView;
+//            }
+//        });
+
+        initListView();
     }
 
     public void createCourses(View v){
@@ -42,10 +73,41 @@ public class MainActivity extends AppCompatActivity {
 //        startCreatingCourses
     }
 
-    private void initRecyclerView(){
-        RecyclerView recyclerView = findViewById(R.id.bottom_recycler_view);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(courseList, this);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    private void initListView(){
+        if(courseList != null) {
+            for (CourseEntity course : courseList) {
+                if (course.getScheduledYear() == 0) {
+                    unscheduledCourses.add(course);
+                }
+            }
+        }
+        final ListView bottomListView = findViewById(R.id.bottom_list_view);
+        CourseArrayAdapter adapter = new CourseArrayAdapter(getApplicationContext(), R.layout.course_layout, unscheduledCourses);
+        bottomListView.setAdapter(adapter);
+        bottomListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(selectedCourse != null)
+                    selectedCourse.setBackground(getDrawable(R.drawable.background_primary_rounded_corners));
+                selectedCourse = view;
+                selectedCourse.setBackground(getDrawable(R.drawable.background_accent_rounded_corners));
+                selectedCoursePosition = position;
+
+            }
+        });
     }
+
+
+    /*
+    todo: the idea right now is that when an item in the bottom ListView gets tapped, it returns the position of that tap
+    to the main activity. when you then tap on another semester's recycler view, this then removes that item from the array
+    of un scheduled courses, adds it to the array of scheduled courses, and enters the scheduled time into the course entity
+
+    the list of scheduled courses is fed to a ListView for the semester, which is dynamically created from one class
+    this ListView adapter is fed the semester and year that it represents in the constructor, and then sorts through the
+    list of scheduled courses to find ones that belong in its display. it then displays them.
+     */
+
+
+
 }
