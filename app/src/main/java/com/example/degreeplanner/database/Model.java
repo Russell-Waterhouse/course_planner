55821dcabf954fb2aad636a78cd13fca16c43639
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2019-12-31 12:20:54 -0800

    created the Observer interface for the model to implement the Observer pattern
    added methods to the Model class to register and notify observers.

--- app/src/main/java/com/example/degreeplanner/database/Model.java
+++ app/src/main/java/com/example/degreeplanner/database/Model.java
@@ -2,12 +2,32 @@ package com.example.degreeplanner.database;
 
 import android.app.Application;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Model extends Repository{
 
     private boolean editing;
+    private List<Observer> observers;
 
     public Model(Application app){
         super(app);
+        observers = new LinkedList<>();
         editing = false;
     }
+
+    public void registerObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    public void modelUpdates(){
+        for(Observer observer : observers){
+            observer.onModelUpdate();
+        }
+    }
+
+    public boolean isEditing(){
+        return isEditing();
+    }
+
 }
---
+++ app/src/main/java/com/example/degreeplanner/database/Observer.java
@@ -0,0 +1,5 @@
+package com.example.degreeplanner.database;
+
+public interface Observer {
+    void onModelUpdate();
+}
