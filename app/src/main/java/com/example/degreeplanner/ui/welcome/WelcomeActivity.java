c9e5baeae955773b3b5d9e7e5cc93ff0a03d3136
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2020-01-04 11:51:04 -0800

    Created a fragment for creating a new schedule. Implemented a spinner in that fragment that allows you to choose your university.
    This new fragment attaches to the Welcome Activity. The necessary edits were thus ade to this file.

--- app/src/main/java/com/example/degreeplanner/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/com/example/degreeplanner/ui/welcome/WelcomeActivity.java
@@ -4,31 +4,52 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import com.example.degreeplanner.R;
 import com.example.degreeplanner.ui.main.MainActivity;
 
-public class WelcomeActivity extends AppCompatActivity implements WelcomeFragment.OnWelcomeFragmentInteractionListener {
+public class WelcomeActivity extends AppCompatActivity
+        implements WelcomeFragment.OnWelcomeFragmentInteractionListener,
+        NewScheduleFragment.OnFragmentInteractionListener {
+    private final String welcomeFragmentTAG = "WELCOME_FRAGMENT";
+    private final String newScheduleFragmentTAG = "NEW_SCHEDULE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         FragmentManager manager = getSupportFragmentManager();
-        manager.beginTransaction().add(R.id.frame, WelcomeFragment.newInstance(), null).commit();
+        manager.beginTransaction().add(R.id.frame, WelcomeFragment.newInstance(), welcomeFragmentTAG).commit();
     }
 
+    @Override
     public void viewScheduleButtonPressed(){
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        finish();
     }
 
+    @Override
     public void createNewScheduleButtonPressed(){
-//        TODO: FInish method
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment currentFragment = manager.findFragmentByTag(welcomeFragmentTAG);
+        if (currentFragment != null) {
+            manager.beginTransaction().hide(currentFragment).add(R.id.frame, NewScheduleFragment.newInstance(), newScheduleFragmentTAG).commit();
+        }
     }
 
+    @Override
     public void aboutAppButtonPressed(){
 //        TODO: FInish implementation
+    }
+
+    @Override
+    public void createScheduleFromScratch() {
+//        TODO: Finish method
+    }
+
+    @Override
+    public void createScheduleFromTemplate() {
+//      TODO: finish method
     }
 }
